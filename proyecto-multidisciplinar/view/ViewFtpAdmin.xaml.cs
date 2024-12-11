3f92d83f74f26e49d8be9337a4015f9ae1db2dcb
jesus-eh <chechuescano981@gmail.com>
2024-12-11 09:58:13 +0100

    VentanaFtp

---
+++ proyecto-multidisciplinar/view/ViewFtpAdmin.xaml.cs
@@ -0,0 +1,41 @@
+using System.Windows;
+
+namespace proyecto_multidisciplinar.view;
+
+public partial class ViewFtpAdmin : Window
+{
+    public ViewFtpAdmin()
+    {
+        InitializeComponent();
+    }
+    public void AccionArchivos(object sender, RoutedEventArgs e)
+    {
+        
+    }
+    public void AccionDirectorio(object sender, RoutedEventArgs e)
+    {
+        
+    }    
+    
+    
+    public void AccionConsultas(object sender, RoutedEventArgs e)
+    {
+        
+    }
+    
+    private void AccionPermisos(object sender, RoutedEventArgs e)
+    {
+    }
+    
+    
+    public void AccionAlmacenamiento(object sender, RoutedEventArgs e)
+    {
+        
+    }
+    
+    public void AccionSalida(object sender, RoutedEventArgs e)
+    {
+        
+    }
+
+}
---
+++ proyecto-multidisciplinar/view/ViewFtpUser.xaml.cs
@@ -0,0 +1,32 @@
+using System.Windows;
+
+namespace proyecto_multidisciplinar.view;
+
+public partial class ViewFtpUser : Window
+{
+    public ViewFtpUser()
+    {
+        InitializeComponent();
+    }
+
+    public void AccionArchivos(object sender, RoutedEventArgs e)
+    {
+        
+    }
+    public void AccionDirectorio(object sender, RoutedEventArgs e)
+    {
+        
+    }    
+    
+    
+    public void AccionConsultas(object sender, RoutedEventArgs e)
+    {
+        
+    }
+    
+    public void AccionSalida(object sender, RoutedEventArgs e)
+    {
+        
+    }
+    
+}
