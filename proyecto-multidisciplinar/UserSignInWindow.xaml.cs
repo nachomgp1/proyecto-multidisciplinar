9f5ebd34c643317c1c1eb8511d778dc3b442512f
sergi <sergiogarcial137@gmail.com>
2024-12-13 12:18:39 +0100

    sing-in hecho(no inserta users jejeje) manda acciones

--- proyecto-multidisciplinar/UserSignInWindow.xaml.cs
+++ proyecto-multidisciplinar/UserSignInWindow.xaml.cs
@@ -1,36 +1,176 @@
-﻿using System;
+﻿using Npgsql;
+using proyecto_multidisciplinar.model;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Security.Policy;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace proyecto_multidisciplinar
 {
-    /// <summary>
-    /// Lógica de interacción para UserSignInWindow.xaml
-    /// </summary>
     public partial class UserSignInWindow : Window
     {
+        public static string radioButtonSelected;
+        public static int userGroup = 0;
+        public static string user=null;
+        public static string email = null;
+        public static string password = null;
+        public static string userType = null;
+
+        Conexion conexion = new Conexion();
+        Dictionary<string, int> groupDictionary = new Dictionary<string, int>(); 
+
         public UserSignInWindow()
         {
             InitializeComponent();
         }
+
+        private void SignIn(object sender, RoutedEventArgs e)
+        {
+             user = userTextBox.Text;
+             email = emailTextBox.Text;
+             password = passwordTextBox.Password;
+             userType = null;
+
+            if (radioButtonSelected.Equals("standardUser"))
+            {
+                userType = "1";
+            }
+            else if (radioButtonSelected.Equals("groupUser"))
+            {
+                userType = "2";
+            }
+            else if (radioButtonSelected.Equals("administrator"))
+            {
+                userType = "3";
+            }
+
+           checkUserData(user, email, password, userType);
+        }
+
+        public void checkUserData(string user, string email, string password, string userType)
+        {
+            if (conexion.AbrirConexion())
+            {
+                try
+                {
+                   
+                    string query = "SELECT * FROM \"Users\" WHERE username = @username OR email = @email";
+
+                    NpgsqlParameter paramUser = new NpgsqlParameter("@username", user);
+                    NpgsqlParameter paramEmail = new NpgsqlParameter("@email", email);
+
+                    NpgsqlDataReader reader = conexion.EjecutarConsulta(query, paramUser, paramEmail);
+                    
+                    if (!reader.HasRows)
+                    {
+                        reader.Close(); 
+
+                        string queryInsert;
+
+                        if (!userType.Equals("2")) 
+                        {
+                            
+                            queryInsert = "INSERT INTO \"Users\" (username, email, password, sent_messages, type, group) " +
+                                          "VALUES (@username, @email, @password, @sentMessages, @type, NULL);";
+
+                            NpgsqlParameter[] parameters = new NpgsqlParameter[] {
+                                 new NpgsqlParameter("@username", user),
+                                new NpgsqlParameter("@email", email),
+                                new NpgsqlParameter("@password", password),
+                                new NpgsqlParameter("@sentMessages", NpgsqlTypes.NpgsqlDbType.Integer) { Value = 0 },
+                                new NpgsqlParameter("@type", userType),
+                                };
+                            MessageBox.Show($"username: {user}, email: {email}, password: {password}, sentMessages: 0, type: {userType}, group: NULL");
+
+                            conexion.EjecutarNonQuery(queryInsert,parameters);
+                            
+                        }
+                        else // Si es tipo grupo
+                        {
+                            queryInsert = "INSERT INTO \"Users\" (username, email, password, sent_messages, type, group) " +
+                                          "VALUES (@username, @email, @password, @sentMessages, @type, @group);";
+                            NpgsqlParameter[] parameters = new NpgsqlParameter[] {
+                                 new NpgsqlParameter("@username", user),
+                                new NpgsqlParameter("@email", email),
+                                new NpgsqlParameter("@password", password),
+                                new NpgsqlParameter("@sentMessages", NpgsqlTypes.NpgsqlDbType.Integer) { Value = 0 },
+                                new NpgsqlParameter("@type", userType),
+                                new NpgsqlParameter("@group", userGroup)
+
+                            };
+                            conexion.EjecutarNonQuery(queryInsert, parameters);
+                            
+                        }
+
+                        conexion.CerrarConexion();
+                        sendAcction("Successfully signed-in");
+                    }
+                    else
+                    {
+                        MessageBox.Show("The username or the email already exists");
+                        reader.Close();
+                        conexion.CerrarConexion();
+                        sendAcction("sign-in error");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error during user creation: " + ex.Message);
+                    conexion.CerrarConexion();
+                   
+                }
+            }
+            else
+            {
+                MessageBox.Show("Could not connect to the database.");
+                
+            }
+        }
+
+        private void sendAcction(string action)
+        {
+            try
+            {
+                // Obtener fecha y hora actual
+                DateTime currentDate = DateTime.Now;
+
+                // Obtener la IP local
+                string userIp = GetLocalIPAddress();
+
+                conexion.AbrirConexion();
+
+                // Query para insertar en Logs
+                string queryInsert = "INSERT INTO \"Logs\" (username, action, date, user_ip) " +
+                                     "VALUES (@username, @action, @date, @user_ip)";
+
+                // Parámetros para la consulta
+                NpgsqlParameter[] parameters = new NpgsqlParameter[] {
+            new NpgsqlParameter("@username", user ?? "Unknown"), // Usar "Unknown" si el usuario no está definido
+            new NpgsqlParameter("@action", action),
+            new NpgsqlParameter("@date", currentDate),
+            new NpgsqlParameter("@user_ip", userIp)
+        };
+
+                // Ejecutar la consulta
+                conexion.EjecutarNonQuery(queryInsert, parameters);
+                conexion.CerrarConexion();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al registrar la acción: " + ex.Message);
+                conexion.CerrarConexion();
+            }
+        }
+
         private void addGroups(object sender, RoutedEventArgs e)
         {
-            
-            String postgreSQL= "postgresql://ProyectoMulti_owner:Xmn5zOt1AZES@ep-solitary-hall-a2pwd4fg.eu-central-1.aws.neon.tech/ProyectoMulti?sslmode=require";
-           
             RadioButton radioButton = sender as RadioButton;
-
+            radioButtonSelected = radioButton.Name;
             ComboBox existingComboBox = null;
+
             foreach (var child in userSignIngGrid.Children)
             {
                 if (child is ComboBox comboBox && comboBox.Name == "groupsComboBox")
@@ -45,7 +185,6 @@ namespace proyecto_multidisciplinar
                 userSignIngGrid.Children.Remove(existingComboBox);
             }
 
-            
             if (radioButton.Name == "groupUser")
             {
                 ComboBox groups = new ComboBox
@@ -53,24 +192,62 @@ namespace proyecto_multidisciplinar
                     Name = "groupsComboBox",
                     Width = 200,
                     Height = 30,
-                    Margin = new Thickness(500, 100, 0, 0)
-                   
+                    Margin = new Thickness(600, -290, 0, 0)
                 };
 
-               //deberia de añadirse mediante la base de datos
+                groups.SelectionChanged += Groups_SelectionChanged; 
 
-                groups.Items.Add("Grupo 1");
-                groups.Items.Add("Grupo 2");
-                groups.Items.Add("Grupo 3");
-                groups.SelectedIndex = 0;
+               
+                conexion.AbrirConexion();
+                string queryGroups = "SELECT id, name FROM \"Groups\"";
+                NpgsqlDataReader reader = conexion.EjecutarConsulta(queryGroups);
+
+                groupDictionary.Clear(); 
+
+                while (reader.Read())
+                {
+                    int id = reader.GetInt32(0); 
+                    string name = reader.GetString(1); 
+                    groupDictionary[name] = id; 
+                    groups.Items.Add(name); 
+                }
+
+                reader.Close();
+                conexion.CerrarConexion();
+
+                groups.SelectedIndex = 0; 
+                if (groups.Items.Count > 0)
+                {
+                    userGroup = groupDictionary[(string)groups.SelectedItem]; 
+                }
+
                 userSignIngGrid.Children.Add(groups);
             }
         }
-            
-           
 
-
+        private void Groups_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+            if (comboBox.SelectedItem != null)
+            {
+                string selectedGroupName = comboBox.SelectedItem.ToString();
+                if (groupDictionary.ContainsKey(selectedGroupName))
+                {
+                    userGroup = groupDictionary[selectedGroupName]; 
+                }
+            }
         }
-        
-    
+        string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            return "127.0.0.1"; // Retorna localhost si no encuentra una IP válida
+        }
+    }
 }
