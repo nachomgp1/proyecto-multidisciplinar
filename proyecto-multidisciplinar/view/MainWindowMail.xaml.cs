fb0e452821c87b10b31254ce08829653762d5d26
samuel <samuelrobles12345@gmail.com>
2024-12-13 12:55:46 +0100

    Agrego funcion de restriccion de mail

--- proyecto-multidisciplinar/view/MainWindowMail.xaml.cs
+++ proyecto-multidisciplinar/view/MainWindowMail.xaml.cs
@@ -38,6 +38,18 @@ namespace proyecto_multidisciplinar.view
                 PrincipalMenuAdmin viewAdmid = new PrincipalMenuAdmin(username);
                 viewAdmid.Show();
             }
+            else if (typeUser == 1)
+            {
+                view.PrincipalMenuStandardUser viewPrincipalMenuUser = new view.PrincipalMenuStandardUser(username);
+                viewPrincipalMenuUser.Show();
+                
+            }
+            else if (typeUser == 2)
+            {
+                view.PrincipalMenuGroupUser viewPrincipalGroupUser = new view.PrincipalMenuGroupUser(username);
+                viewPrincipalGroupUser.Show();
+               
+            }
             this.Close();
         }
 
@@ -47,17 +59,16 @@ namespace proyecto_multidisciplinar.view
             {
                 string credPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../../../resources/credentials.json");
 
-                
                 using (var stream = new FileStream(credPath, FileMode.Open, FileAccess.Read))
                 {
                     string folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), ".credentials/gmail-wpf-quickstart");
 
-                credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
-                        GoogleClientSecrets.Load(stream).Secrets,
-                        Scopes,
-                        "user",
-                        CancellationToken.None,
-                        new FileDataStore(folderPath, true));
+                    credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                            GoogleClientSecrets.Load(stream).Secrets,
+                            Scopes,
+                            "user",
+                            CancellationToken.None,
+                            new FileDataStore(folderPath, true));
                 }
 
                 // Crear servicio de Gmail
@@ -67,10 +78,32 @@ namespace proyecto_multidisciplinar.view
                     ApplicationName = ApplicationName,
                 });
 
-                // Abrir ventana de correos
-                EmailsWindow emailsWindow = new EmailsWindow(service, credential,this);
-                emailsWindow.Show();
-                this.Hide();
+                // Obtener el correo del usuario autenticado
+                var userProfile = await service.Users.GetProfile("me").ExecuteAsync();
+                string authenticatedEmail = userProfile.EmailAddress;
+
+                // Obtener el correo asociado al usuario desde la base de datos
+                string expectedEmail = MainWindow.GetEmail(username);
+
+                // Comparar los correos
+                if (authenticatedEmail.Equals(expectedEmail, StringComparison.OrdinalIgnoreCase))
+                {
+                    // Abrir ventana de correos si los correos coinciden
+                    EmailsWindow emailsWindow = new EmailsWindow(service, credential, this);
+                    emailsWindow.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    // Mostrar error si los correos no coinciden
+                    MessageBox.Show("El correo de Google no coincide con el correo asociado a su cuenta.",
+                        "Error de Autenticación",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Error);
+
+                    // Opcional: Revocar la autorización para limpiar las credenciales
+                    await credential.RevokeTokenAsync(CancellationToken.None);
+                }
             }
             catch (Exception ex)
             {
